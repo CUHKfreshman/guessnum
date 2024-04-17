9cc52eb2e9fc591ec0d3519bc7b2beaa41fc8fab
Liu Chang <70430192+KDLc-design@users.noreply.github.com>
2024-04-18 02:56:44 +0800

    Add files via upload

--- smart_contracts/RandomMatchmaking.sol
+++ smart_contracts/RandomMatchmaking.sol
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.25;
 
-import "@klaytn/contracts/KIP/token/KIP7/IKIP7.sol";
+import "./IERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract RandomMatchmaking {
-    IKIP7 public token;
-    uint256 public constant stakeAmount = 20; // stake amount per player
-    uint256 public constant tokenAmount = 10; // 
-    uint256 public constant roomTimeout = 10 seconds; // max waiting time before timeout
-    uint256 public constant maxNumber = 10; // number range is from 1 to 10
+    IERC20 public token;
+    uint256 public constant stakeAmount = 20; // 每个玩家的赌注数量
+    uint256 public constant tokenAmount = 10; // 代币的单位，按代币的decimals来
+    uint256 public constant roomTimeout = 10 seconds; // 房间超时时间
+    uint256 public constant maxNumber = 10; // 最大猜测数字范围
 
     struct Room {
         address player1;
@@ -30,22 +30,20 @@ contract RandomMatchmaking {
     event GameStarted(uint256 roomNumber, address player1, address player2);
 
     constructor(address _token) {
-        token = IKIP7(_token);
+        token = IERC20(_token);
     }
 
-    /*
-    Check if a room has 2 players (full)
-    */
+    // 检查房间是否活跃（即已满员）
     function isRoomActive(uint256 roomNumber) public view returns (bool) {
         return rooms[roomNumber].isFull;
     }
 
-    /*
-    Called by player to join the game
-    */
+    // 玩家请求加入游戏
     function joinGame(uint256 seed) public {
         require(token.transferFrom(msg.sender, address(this), stakeAmount * tokenAmount), "Failed to transfer tokens");
 
+        // 检查玩家是否已经进入房间
+        require(!isPlayerInRoom(msg.sender), "Already in the Room");
         uint256 roomNumber = findAvailableRoom();
 
         Room storage room = rooms[roomNumber];
@@ -62,15 +60,24 @@ contract RandomMatchmaking {
             emit RoomJoined(roomNumber, msg.sender);
             emit GameStarted(roomNumber, room.player1, room.player2);
 
-            // generate winning numbers based on the seeds provided by players
+            // 使用玩家提供的种子生成隐藏数字
             (room.winningNumber1, room.winningNumber2) = generateWinningNumbers(room.seed1, room.seed2);
         }
     }
 
-    /*
-    Find a free room for a player
-    If no rooms available, create a new one
-    */
+    // 检查玩家是否已经在房间
+    function isPlayerInRoom(address player) public view returns (bool) {
+        for (uint256 i = 1; i <= nextRoomNumber; i++) {
+            Room storage room = rooms[i];
+            if (room.player1 == player || room.player2 == player) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    // 寻找可用房间，如果没有则创建新房间
     function findAvailableRoom() private returns (uint256) {
         for(uint256 i = 1; i < nextRoomNumber; i++) {
             if (!rooms[i].isFull) {
@@ -78,16 +85,14 @@ contract RandomMatchmaking {
             }
         }
 
-        // no avail room found, create a new room
+        // 没有空闲房间，创建一个新的房间
         uint256 newRoomNumber = nextRoomNumber;
         rooms[newRoomNumber] = Room({player1: address(0), player2: address(0), lastActionTime: block.timestamp, isFull: false, winningNumber1: 0, winningNumber2: 0, seed1: 0, seed2: 0});
         nextRoomNumber++;
         return newRoomNumber;
     }
 
-    /*
-    Remove a timed-out room from the mapping
-    */
+    // 删除等待时间超时的房间
     function deleteRoomIfTimeout(uint256 roomNumber) external {
         Room storage room = rooms[roomNumber];
         require(!room.isFull, "Room is full");
@@ -98,32 +103,26 @@ contract RandomMatchmaking {
         resetRoom(roomNumber);
     }
 
-    /*
-    Generate winning numbers based on the seeds provided by players
-    */
+    // 使用种子生成两个隐藏数字
     function generateWinningNumbers(uint256 seed1, uint256 seed2) private pure returns (uint256, uint256) {
         uint256 number1 = uint256(keccak256(abi.encodePacked(seed1, seed2)));
         uint256 number2 = uint256(keccak256(abi.encodePacked(seed2, seed1)));
-        return (number1 % maxNumber + 1, number2 % maxNumber + 1);
+        return (number1 % maxNumber + 1, number2 % maxNumber + 1); // 生成 1 到 maxNumber 之间的数字
     }
 
-    /* 
-    Settlement at the end of a game
-    */
+    // 游戏结算
     function settleGame(uint256 roomNumber, address winner, uint256 prize, address owner, uint256 platformFee) public {
         Room storage room = rooms[roomNumber];
         require(room.isFull, "Room not full");
 
-        // transfer tokens to winner(s)
+        // 转移奖金给赢家
         require(token.transfer(winner, prize), "Failed to transfer winnings to winner");
 
-        // transfer platform profits to contract owner
+        // 转移平台费用给庄家
         require(token.transfer(owner, platformFee), "Failed to transfer platform fee to platform fee receiver");
     }
 
-    /* 
-    Reset a room
-    */
+    // 重置房间
     function resetRoom(uint256 roomNumber) public {
         Room storage room = rooms[roomNumber];
         room.player1 = address(0);
@@ -136,20 +135,18 @@ contract RandomMatchmaking {
         room.seed2 = 0;
     }
 
-    /* 
-    Returns the room number of a player
-    Returns 0 if the player has not been assigned a room
-    */
-    function getPlayerRoomNumber(address player1) public view returns (uint256, address player2) {
+    // 获取房间信息
+    function getPlayerRoomNumber(address player1) public view returns (uint256, address player2, uint256,
+        uint256) {
         for (uint256 i = 1; i < nextRoomNumber; i++) {
             Room storage room = rooms[i];
             if (room.player1 == player1) {
-                return (i, room.player2);
+                return (i, room.player2, room.winningNumber1, room.winningNumber2);
             } else if (room.player2 == player1) {
-                return (i, room.player1);
+                return (i, room.player1, room.winningNumber1, room.winningNumber2);
             }
         }
-        return (0, address(0));
+        return (0, address(0), 0, 0); // 如果玩家不在任何房间中，则返回 0
     }
 
 }
